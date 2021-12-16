7c8891ab87596eed33427b99a11b6f78d8ff718f
aimemo <56360206+aimemo@users.noreply.github.com>
2021-12-17 01:40:58 +0300

    Create testrepos.py

---
+++ .github/workflows/testrepos.py
@@ -0,0 +1,42 @@
+"""
+Test that GitHub repo data dynamically loads the client-side.
+"""
+REPOS = [
+    "find-kedro",
+    "kedro-static-viz",
+    "kedro-action",
+    "steel-toes",
+]
+def test_repos_loaded(slow_driver):
+    """
+    Test that each repo-name exists as a title in one of the repo cards.
+    On waylonwalker.com repo cards have a title with a class of "repo-name"
+    """
+    repos = slow_driver.find_elements_by_class_name("repo-name")
+    # get innertext from elements
+    header_text = [
+        header.text for header in repos
+    ]
+    for repo in REPOS:
+        assert repo in header_text
+def test_repo_description_loaded(slow_driver):
+    """
+    Test that each repo has a description longer than 10 characters
+    On waylonwalker.com repo cards have a descriptiion with a class of "repo-description"
+    """
+    repo_elements = slow_driver.find_elements_by_class_name("repo")
+    for el in repo_elements:
+        desc = el.find_element_by_class_name("repo-description")
+        assert len(desc.text) > 10
+def test_repo_stars_loaded(slow_driver):
+    """
+    Ensure that stars are correctly parsed from the API and loaded client-side
+    On waylonwalker.com repo cards have a stars element with a class of "repo-stars" and
+    is displayed as "n stars"
+    """
+    repo_elements = slow_driver.find_elements_by_class_name("repo")
+    for el in repo_elements:
+        stars = el.find_element_by_class_name("repo-stars")
+        num_stars, label = stars.text.split()
+        assert int(num_stars) > 0
+        assert label == 'stars'
